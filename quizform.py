6b650f8f31d87b9e51384ee0b16b2948865ae524
zpaulinz <zpaulinz@gmail.com>
2025-03-11 02:16:19 +0100

    Update quizform.py

--- quizform.py
+++ quizform.py
@@ -2,13 +2,14 @@ import os
 from PyQt5.QtWidgets import QWidget, QLabel, QLineEdit, QGridLayout
 from PyQt5.QtCore import Qt
 from svg_button import SvgButton
+from form_logic import FormLogic
         
 class QuizForm(QWidget):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("QuizForm")
-        self.setFixedSize(600, 600)
+        self.setFixedSize(500, 580)
 
         self.load_styles()
         self.layout = QGridLayout(self)
@@ -16,8 +17,10 @@ class QuizForm(QWidget):
     
         self.layout.setColumnMinimumWidth(0, 20)  # Column1, min width
         self.layout.setColumnMinimumWidth(3, 20)  # Column,  min width
-
-
+        
+        self.logic = FormLogic(self)
+        
+        
         # Helper functions for creating UI elements
         def create_label(parent, text, object_name, alignment):
             label = QLabel(parent)
@@ -44,7 +47,7 @@ class QuizForm(QWidget):
         self.layout.addWidget(self.header_label, 1, 0, 1, 4)
         
         # Welcome label setup
-        self.welcome_label = create_label(self, "Witaj w Kreatorze pytań i odpowiedzi do aplikacji QuickQuiz!", "welcomeLabel", Qt.AlignCenter)
+        self.welcome_label = create_label(self, "", "welcomeLabel", Qt.AlignLeft)
         self.layout.addWidget(self.welcome_label, 2, 0, 1, 4)
         
         # Info label setup
@@ -53,7 +56,7 @@ class QuizForm(QWidget):
         self.layout.addWidget(self.info_label, 5, 0, 1, 4)
         
         # Filename label setup
-        self.label_filename = create_label(self, "Nazwa pliku (z rozszerzeniem .json):", "filenameLabel", Qt.AlignLeft)
+        self.label_filename = create_label(self, "Nazwa pliku:", "filenameLabel", Qt.AlignLeft)
         self.layout.addWidget(self.label_filename, 3, 1, 1, 2)
         
         # Filename input setup
@@ -61,7 +64,7 @@ class QuizForm(QWidget):
         self.layout.addWidget(self.entry_filename, 4, 1, 1, 2)
 
         # Question label setup
-        self.label_question = create_label(self, "Treść pytania: (1/1)", "questionLabel", Qt.AlignLeft)       
+        self.label_question = create_label(self, "Treść pytania:", "questionLabel", Qt.AlignLeft)       
         self.layout.addWidget(self.label_question, 6, 1, 1, 2)
         
         # Question input setup
@@ -93,23 +96,52 @@ class QuizForm(QWidget):
         self.layout.addWidget(self.entry_incorrect_answer_2, 13,1,1,2)
         
         # Navigation buttons setup
-        self.previous_button = SvgButton("img/previous-no.svg", self)
+        self.previous_button = SvgButton("img/previous-no.svg", "img/previous.svg", "img/previous-active.svg",self)
         self.previous_button.setObjectName("previousButton")
         self.layout.addWidget(self.previous_button, 7,0)
         
-        self.next_button= SvgButton("img/next-no.svg", self)
+        self.next_button= SvgButton("img/next-no.svg", "img/next.svg", "img/next-active.svg",self)
         self.next_button.setObjectName("nextButton")
         self.layout.addWidget(self.next_button, 7,3)
 
         # Save button setup
-        self.save_button = SvgButton("img/save-no.svg", self)
+        self.save_button = SvgButton("img/save-no.svg", "img/save.svg", "img/save-active.svg",self)
         self.save_button.setObjectName("saveButton")
         self.layout.addWidget(self.save_button, 14, 3)
         
         # Footer label setup
-        self.footer_label = create_label(self,'', "footerLabel", Qt.AlignCenter)
-        self.layout.addWidget(self.footer_label, 15,0,1,4)
+        self.footer_label = create_label(self,'', "footerLabel", Qt.AlignLeft)
+        self.layout.addWidget(self.footer_label, 14,0,1,3)
     
+        # Connect to update save button
+        self.entry_filename.textChanged.connect(self.logic.update_save_button)
+        self.entry_question.textChanged.connect(self.logic.update_save_button)
+        self.entry_correct_answer.textChanged.connect(self.logic.update_save_button)
+        self.entry_incorrect_answer_1.textChanged.connect(self.logic.update_save_button)
+        self.entry_incorrect_answer_2.textChanged.connect(self.logic.update_save_button)
+        
+        # Connect to update unsaved questions
+        self.entry_filename.textChanged.connect(self.logic.update_unsaved_questions)
+        self.entry_question.textChanged.connect(self.logic.update_unsaved_questions)
+        self.entry_correct_answer.textChanged.connect(self.logic.update_unsaved_questions)
+        self.entry_incorrect_answer_1.textChanged.connect(self.logic.update_unsaved_questions)
+        self.entry_incorrect_answer_2.textChanged.connect(self.logic.update_unsaved_questions)
+        
+        ## Connect - save file to json by clicked save button
+        self.save_button.clicked.connect(self.logic.save_to_json)
+
+        # Connect to update navigation buttons
+        self.entry_filename.textChanged.connect(self.logic.update_navigation_buttons)
+        self.entry_question.textChanged.connect(self.logic.update_navigation_buttons)
+        self.entry_correct_answer.textChanged.connect(self.logic.update_navigation_buttons)
+        self.entry_incorrect_answer_1.textChanged.connect(self.logic.update_navigation_buttons)
+        self.entry_incorrect_answer_2.textChanged.connect(self.logic.update_navigation_buttons)
+        
+        ## Connect Next and previous buttons
+        self.next_button.clicked.connect(self.logic.next_button_clicked)
+        self.previous_button.clicked.connect(self.logic.previous_button_clicked)
+
+        self.logic.initialize_after_ui_load()
     
     # Load CSS styles from a file   
     def load_styles(self):
